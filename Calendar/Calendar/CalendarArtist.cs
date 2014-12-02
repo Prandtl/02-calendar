4f7dc95aab2857664ad452cf071e10a6c6bf8166
Prandtl <prandtl211@gmail.com>
2014-12-02 19:45:55 +0600

    Working (almost) build need to fix shift in dates

--- Calendar/Calendar/CalendarArtist.cs
+++ Calendar/Calendar/CalendarArtist.cs
@@ -15,28 +15,51 @@ namespace Calendar
             this.month = month;
         }
 
-        public void CreateImage()
+        public void CreateImage()//todo refactor
         {
-            height = 90 + 35*month.AmountOfWeeks;
+            height = 120 + 35*month.AmountOfWeeks;
             width = 500;
 
             calendarPage = new Bitmap(width, height);
 
             var g = Graphics.FromImage(calendarPage);
 
-            g.DrawString(monthNames[month.InputDay.Month], monthName, new SolidBrush(Color.Black), 5, 5);
+            g.DrawString(monthNames[month.InputDay.Month], monthNameFont, new SolidBrush(Color.Black), 5, 5);
             g.DrawString(month.InputDay.Year.ToString(), yearFont, new SolidBrush(Color.Black), width - 80, 60);
 
             int start = width - 70*7;
             foreach (var name in daysHeadNames)
             {
-                g.DrawString(name,daysHead,new SolidBrush(Color.Black),start,85 );
+                g.DrawString(name,daysHeadFont,new SolidBrush(Color.Black),start,85 );
                 start += 65;
             }
-
-
+            
+            DrawDates(g,10,120);
 
             calendarPage.Save("page.bmp");
+        }
+
+        private void DrawDates(Graphics g,int xStart,int yStart)
+        {
+
+            var dates = Enumerable.Repeat(0, (int) month.FirstDay - 1)
+                .Concat(Enumerable.Range(1, month.AmountOfDays)).ToArray();//todo:добавляет слишком много нулей в начале
+            if (month.DaysOnLastWeek > 0)
+                dates=dates.Concat(Enumerable.Repeat(0, 7 - month.DaysOnLastWeek)).ToArray();
+            var weeks = dates.Select((x, i) => Tuple.Create(x, i/7))
+                .GroupBy(x => x.Item2)
+                .Select(x=>x.Select(z=>z.Item1)
+                    .ToArray())
+                .ToArray();
+            for (int i = 0; i < weeks.Length; i++)
+            {
+                for (int j = 0; j < weeks[i].Length; j++)
+                {
+                    if(weeks[i][j]==0)
+                        continue;
+                    g.DrawString(weeks[i][j].ToString(), daysFont, simpleBlackBrush, xStart+j*65, yStart+i*35);
+                }
+            }
         }
 
         private Bitmap calendarPage;
@@ -46,9 +69,12 @@ namespace Calendar
         private int width = 500;
         private int height = 90 + 35*6;
 
-        private Font monthName = new Font(FontFamily.GenericMonospace, 50, FontStyle.Bold);
+        private Font monthNameFont = new Font(FontFamily.GenericMonospace, 50, FontStyle.Bold);
         private Font yearFont = new Font(FontFamily.GenericSansSerif, 20);
-        private Font daysHead = new Font(FontFamily.GenericMonospace, 30);
+        private Font daysHeadFont = new Font(FontFamily.GenericMonospace, 30);
+        private Font daysFont = new Font(FontFamily.GenericMonospace, 30);
+
+        private Brush simpleBlackBrush=new SolidBrush(Color.Black);
 
         private Dictionary<int, string> monthNames = new Dictionary<int, string>()
         {
--- Calendar/Calendar/Month.cs
+++ Calendar/Calendar/Month.cs
@@ -12,27 +12,28 @@ namespace Calendar
         public int FirstWeekNumber { get; private set; }
         public int AmountOfWeeks { get; private set; }
         public int AmountOfDays { get; private set; }
+        public int DaysOnLastWeek { get; private set; }
         public DayOfWeek FirstDay { get; private set; }
         public DateTime InputDay { get; private set; }
 
-        public Month(int day,int month,int year)
+        public Month(int day, int month, int year)
         {
-            InputDay=new DateTime(year,month,day,new GregorianCalendar());
+            InputDay = new DateTime(year, month, day, new GregorianCalendar());
             System.Globalization.Calendar cal = new GregorianCalendar();
             var first = new DateTime(year, month, 1);
             FirstDay = cal.GetDayOfWeek(first);
             AmountOfDays = cal.GetDaysInMonth(year, month);
             int firstDay = (int) FirstDay - 1;
             int daysWithoutFirstWeek = AmountOfDays - (7 - firstDay);
-            int daysOnLastWeek = daysWithoutFirstWeek%7;
-            AmountOfWeeks = daysOnLastWeek == 0 ? 
-                daysWithoutFirstWeek/7 + 1 :
-                daysWithoutFirstWeek/7 + 2;
-            FirstWeekNumber = cal.GetWeekOfYear(first, 
+            DaysOnLastWeek = daysWithoutFirstWeek % 7;
+            AmountOfWeeks = DaysOnLastWeek == 0
+                ? daysWithoutFirstWeek/7 + 1
+                : daysWithoutFirstWeek/7 + 2;
+            FirstWeekNumber = cal.GetWeekOfYear(first,
                 DateTimeFormatInfo.CurrentInfo.CalendarWeekRule,
                 DateTimeFormatInfo.CurrentInfo.FirstDayOfWeek);
         }
 
-        
+
     }
 }
